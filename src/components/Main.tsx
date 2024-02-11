789cd0305100298c7c53277dc35e0bc17761eaed
Chadi RABII <chadirabii@gmail.com>
2024-02-11 02:25:22 +0100

    Add state and functionality to the "No" button in Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import Button from "./Button";
 
 function Main() {
+  const [noClicked, setNoClicked] = useState(false);
+  const [yesButtonSize, setYesButtonSize] = useState(16);
+
+  const handleNoButtonClick = () => {
+    console.log("No");
+    setNoClicked(true);
+    setYesButtonSize((prevSize) => prevSize + 2);
+  };
+
   return (
     <>
-      <Button name="Yes" onClick={() => console.log("Yes")} />
-      <Button name="No" onClick={() => console.log("No")} />
+      <Button
+        name="Yes"
+        className={noClicked ? "bigger" : ""}
+        style={{ fontSize: `${yesButtonSize}px` }}
+        onClick={() => {
+          console.log("Yes");
+        }}
+      />
+      <Button name="No" onClick={handleNoButtonClick} />
     </>
   );
 }
