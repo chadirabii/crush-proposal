82d34f56ac15a782c1e99731bfb2716db6696cb7
Chadi RABII <chadirabii@gmail.com>
2024-02-11 02:23:05 +0100

    Add onClick and style props to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,26 @@
+import { CSSProperties } from "react";
+
 type ButtonProps = {
   name: string;
+  onClick: () => void;
+  className?: string;
+  style?: CSSProperties; // Add style here
 };
 
-function Button({ name }: ButtonProps) {
+function Button({ name, onClick, className = "", style }: ButtonProps) {
   const buttonColor =
     name === "YES" || name === "Yes" ? "bg-green-500" : "bg-red-500";
 
+  const handleClick = () => {
+    onClick();
+  };
+
   return (
     <button
-      className={`px-4 py-2 rounded-md text-white ${buttonColor}`}
+      className={`rounded-md text-white ${buttonColor} px-4 py-2 ${className}`} // Use className here
       type="button"
+      onClick={handleClick}
+      style={style}
     >
       {name}
     </button>
