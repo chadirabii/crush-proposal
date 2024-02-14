e43555a8ad887b9633c1c4de74f858478f001783
Chadi RABII <chadirabii@gmail.com>
2024-02-14 20:01:02 +0100

    Remove unused import statement

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Button from "./Button";
 import GifDisplay from "./GifDisplay";
 import please from "../assets/please.gif";
